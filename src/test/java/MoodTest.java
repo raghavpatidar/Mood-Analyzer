6a747119e74d1b03e23dbbd88fc623b85cf3a4c9
raghavpatidar <raghavpatidar997764@gmail.com>
2024-01-02 21:24:48 +0530

    Test3.1 and 3.2 done

--- src/test/java/MoodTest.java
+++ src/test/java/MoodTest.java
@@ -1,29 +1,38 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import com.example.MoodAnalysisException;
 import com.example.MoodAnalyzer;
 
 public class MoodTest {
+
     @Test
-    public void moodAnalyzerTestUC11() {
+    public void moodAnalyzerTestUC11() throws MoodAnalysisException {
         String msg = "I am in Sad Mood";
-        MoodAnalyzer<String> moodAnalyzer = new MoodAnalyzer<String>(msg);
+        MoodAnalyzer<String> moodAnalyzer = new MoodAnalyzer<>(msg);
         assertEquals("Sad", moodAnalyzer.analyseMood());
     }
 
     @Test
-    public void moodAnalyzerTestUC12() {
+    public void moodAnalyzerTestUC12() throws MoodAnalysisException {
         String msg = "I am in Any Mood";
-        MoodAnalyzer<String> moodAnalyzer = new MoodAnalyzer<String>(msg);
+        MoodAnalyzer<String> moodAnalyzer = new MoodAnalyzer<>(msg);
         assertEquals("Happy", moodAnalyzer.analyseMood());
     }
 
     @Test
-    public void moodAnalyzerTestUC21() {
+    public void moodAnalyzerTestUC21() throws MoodAnalysisException {
         String msg = null;
-        MoodAnalyzer<String> moodAnalyzer = new MoodAnalyzer<String>(msg);
-        assertEquals("Happy", moodAnalyzer.analyseMood());
+        MoodAnalyzer<String> moodAnalyzer = new MoodAnalyzer<>(msg);
+        assertThrows(MoodAnalysisException.class, () -> moodAnalyzer.analyseMood());
     }
 
+    @Test
+    public void moodAnalyzerTestUC22() {
+        String msg = "";
+        MoodAnalyzer<String> moodAnalyzer = new MoodAnalyzer<>(msg);
+        assertThrows(MoodAnalysisException.class, () -> moodAnalyzer.analyseMood());
+    }
 }
